99e48237a4b5990e4a621bb4994c720f9e342960
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-01-18 16:32:50 +0530

    Create StringBuffer.java

---
+++ StringBuffer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        StringBuffer a=new StringBuffer("hello this is lakshana");
+        System.out.println(a.reverse());
+        System.out.println(a.substring(0,9));
+        System.out.println(a.codePointAt(7));
+        System.out.println(a.capacity());
+        System.out.println(a.append("bye.!"));
+    }
+}
