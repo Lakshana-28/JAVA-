ab032894f13e1b87c1fd648600fd39146058e618
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-01-18 16:45:16 +0530

    Create java contains method.java

---
+++ java contains method.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String a=new String("hello this is lakshana");
+        System.out.println(a.contains("hello"));
+        
+    }
+}
